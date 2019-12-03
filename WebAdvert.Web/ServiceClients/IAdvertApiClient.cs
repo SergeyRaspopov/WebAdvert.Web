89f2b34b7f6c7141a3c31b20514f2187f9d60d9f
SR <raspopov.sergey@gmail.com>
2019-12-02 22:56:20 -0800

    6.24 in progress

---
+++ WebAdvert.Web/ServiceClients/IAdvertApiClient.cs
@@ -0,0 +1,14 @@
+﻿using AdvertApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.ServiceClients
+{
+    public interface IAdvertApiClient
+    {
+        Task<CreateAdvertResponse> Create(AdvertModel model);
+    }
+}
